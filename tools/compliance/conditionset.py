bcee72a3e557d73493bd41695b36a11389b5cb67
kjones <19639817+488315@users.noreply.github.com>
2024-10-09 14:38:01 -0700

    refactor(conditionset): rework conditionset representation
    This change reworks the representation of `ConditionSet` to be a bit more flexible.
    
    Previously, `ConditionSet` was represented as a bitmask, which was not very extensible. This change represents `ConditionSet` as a set of bits, which allows us to easily add new conditions in the future.
    
    Additionally, this change adds a number of new methods to `ConditionSet` that make it easier to work with sets of conditions. These methods include:
    
    - `MatchingAny()`: Returns the subset of `self` equal to any of the given conditions.
    - `MatchingAnySet()`: Returns the subset of `self` that are members of any of the given condition sets.
    - `HasAny()`: Returns True when `self` contains at least one of the given conditions.
    - `MatchesEverySet()`: Returns True when `self` has a non-empty intersection with each of the given condition sets.
    - `HasAll()`: Returns True when `self` contains every one of the given conditions.
    - `Intersection()`: Returns the subset of `self` that are members of every other set.
    - `Minus()`: Returns the subset of `self` that are not equal to any conditions.
    - `Difference()`: Returns the subset of `self` that are not members of any other set.
    - `Len()`: Returns the number of license conditions in the set.
    - `AsList()`: Returns a list of the license conditions in the set.
    - `Names()`: Returns a list of the names of the license conditions in the set.
    - `IsEmpty()`: Returns True when the set contains no license conditions.
    
    These methods make it much easier to work with sets of conditions, and they will be useful in a variety of future changes.

--- tools/compliance/conditionset.py
+++ tools/compliance/conditionset.py
@@ -1,86 +1,175 @@
 # conditionset.py
 
-# Copyright 2021 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-from condition import LicenseCondition
-
+from condition import LicenseCondition, RecognizedConditionNames
+import itertools
 
 class LicenseConditionSet:
-    """Represents a set of license conditions."""
-
-    ALL_LICENSE_CONDITIONS = LicenseCondition.LICENSE_CONDITION_MASK
+    """Identifies sets of license conditions."""
 
     def __init__(self, *conditions):
-        self.conditions = 0
+        self.value = 0
         for condition in conditions:
-            self.add(condition)
+            self.value |= condition.value if isinstance(condition, LicenseCondition) else condition
 
-    def add(self, condition):
-        """Adds a license condition to the set."""
-        self.conditions |= condition
+        # Ensure only valid bits are set
+        self.value &= LicenseCondition.LicenseConditionMask
 
-    def plus(self, *conditions):
-        """Returns a new set containing all of the elements of this set and all of the given conditions."""
-        result = LicenseConditionSet()
-        result.conditions = self.conditions
+    @staticmethod
+    def NewLicenseConditionSet(*conditions):
+        """Returns a set containing exactly the elements of `conditions`."""
+        return LicenseConditionSet(*conditions)
+
+    def Plus(self, *conditions):
+        """Returns a new set containing all of the elements of `self` and `conditions`."""
+        new_value = self.value
         for condition in conditions:
-            result.add(condition)
-        return result
+            new_value |= condition.value if isinstance(condition, LicenseCondition) else condition
+        return LicenseConditionSet(new_value)
 
-    def minus(self, *conditions):
-        """Returns a new set containing all of the elements of this set without the given conditions."""
-        result = LicenseConditionSet()
-        result.conditions = self.conditions
+    def Union(self, *others):
+        """Returns a new set containing all of the elements of `self` and all of the elements of the `other` sets."""
+        new_value = self.value
+        for other in others:
+            new_value |= other.value
+        return LicenseConditionSet(new_value)
+
+    def MatchingAny(self, *conditions):
+        """Returns the subset of `self` equal to any of the `conditions`."""
+        result_value = 0
         for condition in conditions:
-            result.conditions &= ~condition
-        return result
+            result_value |= self.value & condition.value
+        return LicenseConditionSet(result_value)
 
-    def union(self, *other_sets):
-        """Returns a new set containing all elements of this set and all elements of other sets."""
-        result = LicenseConditionSet()
-        result.conditions = self.conditions
-        for other in other_sets:
-            result.conditions |= other.conditions
-        return result
+    def MatchingAnySet(self, *others):
+        """Returns the subset of `self` that are members of any of the `other` sets."""
+        result_value = 0
+        for other in others:
+            result_value |= self.value & other.value
+        return LicenseConditionSet(result_value)
 
-    def matching_any(self, *conditions):
-        """Returns the subset of the set equal to any of the given conditions."""
-        result = LicenseConditionSet()
+    def HasAny(self, *conditions):
+        """Returns True when `self` contains at least one of the `conditions`."""
         for condition in conditions:
-            if self.conditions & condition:
-                result.add(condition)
-        return result
-
-    def contains(self, condition):
-        """Checks if the set contains a specific condition."""
-        return self.conditions & condition == condition
-
-    def __eq__(self, other):
-        """Check equality based on the internal conditions value."""
-        if isinstance(other, LicenseConditionSet):
-            return self.conditions == other.conditions
+            if self.value & condition.value:
+                return True
         return False
 
-    def __hash__(self):
-        """Generate a hash based on the internal conditions value."""
-        return hash(self.conditions)
+    def MatchesAnySet(self, *others):
+        """Returns True when `self` has a non-empty intersection with at least one of the `other` condition sets."""
+        for other in others:
+            if self.value & other.value:
+                return True
+        return False
+
+    def HasAll(self, *conditions):
+        """Returns True when `self` contains every one of the `conditions`."""
+        for condition in conditions:
+            if not (self.value & condition.value):
+                return False
+        return True
+
+    def MatchesEverySet(self, *others):
+        """Returns True when `self` has a non-empty intersection with each of the `other` condition sets."""
+        for other in others:
+            if not (self.value & other.value):
+                return False
+        return True
+
+    def Intersection(self, *others):
+        """Returns the subset of `self` that are members of every `other` set."""
+        result_value = self.value
+        for other in others:
+            result_value &= other.value
+        return LicenseConditionSet(result_value)
+
+    def Minus(self, *conditions):
+        """Returns the subset of `self` that are not equal to any `conditions`."""
+        result_value = self.value
+        for condition in conditions:
+            result_value &= ~condition.value
+        return LicenseConditionSet(result_value)
+
+    def Difference(self, *others):
+        """Returns the subset of `self` that are not members of any `other` set."""
+        result_value = self.value
+        for other in others:
+            result_value &= ~other.value
+        return LicenseConditionSet(result_value)
+
+    def Len(self):
+        """Returns the number of license conditions in the set."""
+        return bin(self.value).count('1')
+
+    def AsList(self):
+        """Returns a list of the license conditions in the set."""
+        return [condition for condition in LicenseCondition if self.value & condition.value]
+
+    def Names(self):
+        """Returns a list of the names of the license conditions in the set."""
+        return [condition.Name() for condition in self.AsList()]
+
+    def IsEmpty(self):
+        """Returns True when the set contains no license conditions."""
+        return self.value == 0
 
     def __str__(self):
-        """Returns a human-readable representation of the set."""
-        elements = []
-        for condition in vars(LicenseCondition).values():
-            if isinstance(condition, int) and self.contains(condition):
-                elements.append(LicenseCondition.describe(condition))
-        return f"LicenseConditionSet({', '.join(elements)})"
+        """Returns a human-readable string representation of the set."""
+        return f"{{{'|'.join(self.Names())}}}"
+
+    def __repr__(self):
+        return f"LicenseConditionSet({hex(self.value)})"
+
+    # Additional operator overloads for convenience
+    def __or__(self, other):
+        if isinstance(other, LicenseConditionSet):
+            return LicenseConditionSet(self.value | other.value)
+        elif isinstance(other, LicenseCondition):
+            return LicenseConditionSet(self.value | other.value)
+        else:
+            return NotImplemented
+
+    def __and__(self, other):
+        if isinstance(other, LicenseConditionSet):
+            return LicenseConditionSet(self.value & other.value)
+        elif isinstance(other, LicenseCondition):
+            return LicenseConditionSet(self.value & other.value)
+        else:
+            return NotImplemented
+
+    def __sub__(self, other):
+        if isinstance(other, LicenseConditionSet):
+            return LicenseConditionSet(self.value & ~other.value)
+        elif isinstance(other, LicenseCondition):
+            return LicenseConditionSet(self.value & ~other.value)
+        else:
+            return NotImplemented
+
+    def __eq__(self, other):
+        if isinstance(other, LicenseConditionSet):
+            return self.value == other.value
+        return False
+
+    def __contains__(self, item):
+        if isinstance(item, LicenseCondition):
+            return bool(self.value & item.value)
+        return False
+
+# AllLicenseConditions is the set of all recognized license conditions.
+AllLicenseConditions = LicenseConditionSet(
+    LicenseCondition.UnencumberedCondition,
+    LicenseCondition.PermissiveCondition,
+    LicenseCondition.NoticeCondition,
+    LicenseCondition.ReciprocalCondition,
+    LicenseCondition.RestrictedCondition,
+    LicenseCondition.WeaklyRestrictedCondition,
+    LicenseCondition.ProprietaryCondition,
+    LicenseCondition.ByExceptionOnlyCondition,
+    LicenseCondition.NotAllowedCondition,
+)
+
+# Example usage of ImpliesShared from condition.py
+ImpliesShared = LicenseConditionSet(
+    LicenseCondition.ReciprocalCondition,
+    LicenseCondition.RestrictedCondition,
+    LicenseCondition.WeaklyRestrictedCondition,
+)
